5769609c379e171d77638c9db6daf4da7b4414b5
Algernon1994 <186277735+Algernon1994@users.noreply.github.com>
2024-10-25 14:55:32 +0100

    Think i've got it right, works at least

--- text_to_mc.py
+++ text_to_mc.py
@@ -9,3 +9,24 @@ text_to_morse_code = {
     '(': '-.--.', ')': '-.--.-', '&': '.-...', ':': '---...', ';': '-.-.-.', '=': '-...-', 
     '+': '.-.-.', '-': '-....-', '_': '..--.-', '"': '.-..-.', '$': '...-..-', '@': '.--.-.'
 }
+
+def text_to_morse(words):
+    morse_code = []
+    for word in words:
+        morse_word = []
+        for char in word.upper():
+            if char in text_to_morse_code:
+                morse_word.append(text_to_morse_code[char])  # converts each character to morse code
+        morse_code.append(' '.join(morse_word))  # put a space between each letter
+    return '   '.join(morse_code)  # puts three spaces between words, if i haven't misscounted
+
+# let someone type words until they type 'done' like i learned from either yesterday or last week
+words = []
+while True:
+    word = input("Enter a word to convert to Morse code (type 'done' to finish): ")
+    if word.lower() == 'done':
+        break
+    words.append(word)
+
+morse_result = text_to_morse(words)
+print("Morse Code:", morse_result)
